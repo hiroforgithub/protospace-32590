f30fc9a6c9ed5b2abdf9ee8d9800a4c17e5897a7
hiroforgithub <herohiro505@gmail.com>
2020-12-15 14:30:47 +0900

    アクションとルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
+  resources :prototypes, only: [:new, :create]
 
   get "login" => "sessions#new"
   get "login" => "sessions#create"
