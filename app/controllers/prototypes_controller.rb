554d04bf5fa3c3a2f84a2bf1126480ac70349386
hiroforgithub <herohiro505@gmail.com>
2020-12-16 11:31:38 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,8 +4,28 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+    
+  end
+  
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :image, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
